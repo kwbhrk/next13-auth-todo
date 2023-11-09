0b1acf97566221092b9cf4412e379d41cd41189a
hirochandayo <hirokuwa0323@gmail.com>
2023-11-09 09:03:32 +0900

    update: ログイン時のemailのバリデーションルールを一部変更

--- app/schema/login.ts
+++ app/schema/login.ts
@@ -1,9 +1,6 @@
 import * as z from 'zod';
 
 export const loginSchema = z.object({
-  email: z
-    .string()
-    .email({ message: 'メールアドレスの形式ではありません。' })
-    .min(1, { message: '入力されていません。' }),
+  email: z.string().min(1, { message: '入力されていません。' }),
   password: z.string().min(1, { message: '入力されていません。' }),
 });
