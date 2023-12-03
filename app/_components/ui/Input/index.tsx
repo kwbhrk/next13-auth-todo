80de3fa9ae403ba278ba1658d46c60df937d610c
hirochandayo <hirokuwa0323@gmail.com>
2023-12-03 13:42:06 +0900

    update: InputコンポーネントにonChangeイベントを明示的に追加

--- app/_components/ui/Input/index.tsx
+++ app/_components/ui/Input/index.tsx
@@ -3,6 +3,8 @@ import type { InputProps } from './type';
 import { InputClass } from './style';
 
 // eslint-disable-next-line react/display-name
-export const Input = forwardRef<HTMLInputElement, InputProps>(({ fullWidth, ...props }, ref) => (
-  <input {...props} ref={ref} className={InputClass({ fullWidth })} />
-));
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ fullWidth, onChange, ...props }, ref) => (
+    <input {...props} ref={ref} className={InputClass({ fullWidth })} onChange={onChange} />
+  )
+);
--- app/_components/ui/Input/type.ts
+++ app/_components/ui/Input/type.ts
@@ -1,1 +1,5 @@
-export type InputProps = { fullWidth?: boolean } & React.JSX.IntrinsicElements['input'];
+export type InputProps = {
+  fullWidth?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onChange?: <T extends { target: any; type?: any }>(arg: T) => void;
+} & React.JSX.IntrinsicElements['input'];
